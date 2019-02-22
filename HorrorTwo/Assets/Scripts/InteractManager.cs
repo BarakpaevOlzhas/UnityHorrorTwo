ecc735c40c9480c4a54150e0731befe8d008e157
BarakpaevOlzhas <olzhas.barakpaev@mail.ru>
2019-02-22 19:04:24 +0600

    Add new project

---
+++ HorrorTwo/Assets/Scripts/InteractManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractManager : MonoBehaviour
+{
+    [SerializeField]
+    private float interactDistance;
+
+    [SerializeField]
+    private Transform cameraPosition;
+
+    [SerializeField]
+    private FlashLight flashLight;
+
+    void Start()
+    {
+        
+    }
+   
+    void Update()
+    {
+        Ray ray = new Ray(cameraPosition.position, cameraPosition.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                if (hit.collider.tag == "Battery")
+                {
+                    flashLight.AddEnergy(1.5f);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
